27b300d5d0d75d86c658987be5447e47a43a64d5
rubenruizga <e9.ruben@gmail.com>
2018-03-19 13:29:47 -0700

    Plus topbar

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,16 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+
+//TopBar
+function dropIt() {
+    document.getElementById("mobile-menu").style.top='0';
+}
+function pickIt() {
+    document.getElementById("mobile-menu").style.top='-100vh';
+}
+document.getElementById("mobile-menu-button").addEventListener ("click", dropIt, false);
+document.getElementById("mobile-menu-button-close").addEventListener ("click", pickIt, false);
+document.getElementById("mobile-menu-item-1").addEventListener ("click", pickIt, false);
+document.getElementById("mobile-menu-item-2").addEventListener ("click", pickIt, false);
+document.getElementById("mobile-menu-item-4").addEventListener ("click", pickIt, false);
