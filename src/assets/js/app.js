28074fea8e9c93e7ca2c6b50d3376d36e35b9471
rubenruizga <e9.ruben@gmail.com>
2018-03-20 22:28:47 -0700

    Fetching projects from firebase

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,16 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-
+import firebase from 'firebase';
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD6to7A2fqVY_MGoy29U-c9HKhXXWwaYAA",
+  authDomain: "benijan-e382e.firebaseapp.com",
+  databaseURL: "https://benijan-e382e.firebaseio.com",
+  projectId: "benijan-e382e",
+  storageBucket: "",
+  messagingSenderId: "930711717630"
+};
+firebase.initializeApp(config);
 window.$ = $;
 
 import Foundation from 'foundation-sites';
@@ -23,3 +33,40 @@ document.getElementById("mobile-menu-button-close").addEventListener ("click", p
 document.getElementById("mobile-menu-item-1").addEventListener ("click", pickIt, false);
 document.getElementById("mobile-menu-item-2").addEventListener ("click", pickIt, false);
 document.getElementById("mobile-menu-item-4").addEventListener ("click", pickIt, false);
+
+//Portfolio
+firebase.database().ref('/sites/tqw/portfolio').once('value').then(function(snapshot) {
+  var projects = snapshot.val();
+  var portfolio = document.getElementById("portfolio-projects");
+  Object.keys(projects).map(function(key, index) {
+    var cell = document.createElement('div');
+    var portfolioItem = document.createElement('div');
+
+    var portfolioItemImg = document.createElement('div');
+    var projectImg = document.createElement('img');
+
+    var portfolioItemDescription = document.createElement('div');
+    var projectName = document.createElement('h5');
+    var projectType = document.createElement('h6');
+
+    cell.className = 'cell medium-6 large-4';
+    portfolioItem.className = 'portfolio-item';
+    portfolioItemImg.className = 'portfolio-item-img';
+    portfolioItemDescription.className = 'portfolio-item-description';
+
+    var type = document.createTextNode(projects[key].type);
+    var name = document.createTextNode(projects[key].name);
+
+    projectName.append(name);
+    projectType.append(type);
+    projectImg.src = projects[key].previewImg;
+
+    portfolioItemImg.append(projectImg);
+    portfolioItemDescription.append(projectType);
+    portfolioItemDescription.append(projectName);
+    portfolioItem.append(portfolioItemImg);
+    portfolioItem.append(portfolioItemDescription);
+    cell.append(portfolioItem);
+    portfolio.append(cell);
+  });
+});
